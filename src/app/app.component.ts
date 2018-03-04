210a1d7594845d0b4f26276bd754cc8e6968d2a4
Mladen <djoml8bre@gmail.com>
2018-03-04 19:52:35 +0100

    Uploading a file and displaying progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedFile: File = null;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onUpload() {
+    const formData = new FormData();
+    formData.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('localhost://8000/api/files', formData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe((event) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          console.log('Upload Progress:' + Math.round(event.loaded / event.total * 100) + '%');
+        } else if (event.type === HttpEventType.Response) {
+          console.log(event);
+        }
+      });
+  }
 }
